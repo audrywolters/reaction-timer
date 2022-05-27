1a051e8fececcbad1456ceaac15a78a129fb647b
Audry <audrywolters@gmail.com>
2022-05-27 16:51:57 -0500

    added rank display

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,13 +1,32 @@
 <template>
   <p>Reaction Time: {{ score }} ms</p>
+  <p class="rank">{{ rank }}</p>
 </template>
 
 <script>
 export default {
-    props: ['score']
+    props: ['score'],
+    data() {
+        return {
+            rank: null
+        }
+    },
+    mounted() {
+        if (this.score < 250) {
+            this.rank = 'Ninja Click'
+        } else if (this.score < 400) {
+            this.rank = 'Rapid Reflex'
+        } else {
+            this.rank = 'Slowish'
+        }
+    }
 }
 </script>
 
 <style>
-
+.rank {
+    font-size: 1.4em;
+    color: #0faf87;
+    font-weight: bold;
+}
 </style>
